43f0522abcc6170fe5165f7421f42116511c4399
Maryam Abid <hey.maryam12@gmail.com>
2026-08-05 21:37:24 +0500

    LOGIN

--- Backend/Routes/Auth.js
+++ Backend/Routes/Auth.js
@@ -4,11 +4,14 @@ const { body, validationResult } = require('express-validator');
 const User = require('../models/User');
 const bcrypt = require("bcryptjs");
 const jwt= require('jsonwebtoken');
+const dotenv=require ('dotenv');
+// reads the .env file and loads the variables into process .env 
+dotenv.config();
 
 
 
 
-// post req for sending data to the server
+// post req for sending data to the server(creating a user)
 router.post('/',
   // adding validation
   [
@@ -16,7 +19,8 @@ router.post('/',
     body('email', 'Please provide a valid email').isEmail(),
     body('password', 'Password must be at least 5 characters long').isLength({ min: 5 })
   ], async (req, res) => {
-    const secretKey ="maryam@123";
+    const secretKey =process.env.secretKey;
+    // validating the request body data 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() })
@@ -33,16 +37,66 @@ router.post('/',
     const pasword = await bcrypt.hash(req.body.password, salt);
 
 
-
+   // user creation using mongoose model
     const user = await User.create({
       name: req.body.name,
       email: req.body.email,
       password: pasword
     });
 
+    // sending the token to the user
     res.send(token)
 
 
 
   })
+   
+ // authentification of user api/auth/login
+ router.post('/login',[
+   body('email', 'Please provide a valid email').isEmail(),
+  body('password', 'Password must be provided').exists()],
+  async(req,res)=>{
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+  
+    // deconstructing    
+    const {email,password}=req.body;
+    // getting secret key from the .env file
+    const secretkey=process.env.secretkey;
+    try{
+
+    const user=await User.findOne({email});
+    if(!user){
+      return res.status(400).json({error:"Please try to login with correct credentials"});
+    }
+
+    // comparing the password
+    const passwordcheck =await bcrypt.compare(password,user.password);
+    if(!passwordcheck){
+      return res.status(400).json({error:"Please try to login with correct credentials"});
+    }
+    // creating a token for the user
+    const token=jwt.sign({id:user.id},secretkey,{expiresIn:"1h"});
+
+    res.send(token);
+  }catch(err){
+    console.error(err.message);
+    res.status(500).send("Internal server error");
+  }
+
+   
+
+
+
+
+ })
+
+
+
+
+
 module.exports = router
